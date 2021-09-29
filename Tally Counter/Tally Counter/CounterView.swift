5330ebeb993ead795af78e42dd8fc0707dcc1b0a
vfil <wladyslawfil@gmail.com>
2021-09-29 10:17:54 +0300

    Add colors

---
+++ Tally Counter/Tally Counter/CounterView.swift
@@ -0,0 +1,22 @@
+//
+//  CounterView.swift
+//  Tally Counter
+//
+//  Created by  Vladyslav Fil on 29.09.2021.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+        }
+    }
+}
+
+struct CounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CounterView()
+    }
+}
