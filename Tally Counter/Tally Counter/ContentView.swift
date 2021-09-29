0ede661fd90022b7563357bf09a8472844f4cd2d
vfil <wladyslawfil@gmail.com>
2021-09-29 09:38:08 +0300

    Create project

---
+++ Tally Counter/Tally Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tally Counter
+//
+//  Created by  Vladyslav Fil on 29.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
